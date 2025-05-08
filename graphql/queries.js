dccb3514e758137a09d8e26fc91b185bd6eadb92
ebarsula <ebarsulai@gmail.com>
2025-05-08 10:19:33 +0300

    feat(graphql): refine GET_USER_INFO query to fetch xp transactions and project progresses by event ID

--- graphql/queries.js
+++ graphql/queries.js
@@ -1,20 +1,32 @@
-const GET_USER_INFO = `
-  query getUserProfile($eventId: Int!) {
-    user {
-      login
-      attrs
-      auditRatio
-      transactions(where: {eventId: {_eq: $eventId}}, order_by: {createdAt: asc}) {
-        amount
-        createdAt
-        path
-        type
+    const GET_USER_INFO = `
+      query getUserProfile($eventId: Int!) {
+        user {
+          login
+          attrs
+          auditRatio
+          transactions(
+            where: { type: { _eq: "xp" } }
+            order_by: { createdAt: asc }
+          ) {
+            amount
+            createdAt
+            path
+            type
+          }
+          progresses(
+            where: { eventId: { _eq: $eventId } }
+            order_by: { createdAt: desc }
+          ) {
+            grade
+            path
+            createdAt
+            object {
+              id
+              name
+              type
+              attrs 
+            }
+          }
+        }
       }
-      progresses(where: {eventId: {_eq: $eventId}}, order_by: {createdAt: desc}) {
-        grade
-        path
-        createdAt
-      }
-    }
-  }
-`;
+    `;
